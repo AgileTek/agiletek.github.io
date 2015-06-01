94ee6fcd1ba6a2c8d862c44889ca489ad2000efc
srossiter <steve@agiletek.co.uk>
2015-06-01 14:17:43 +0100

    change task

--- tasks.py
+++ tasks.py
@@ -1,16 +1,16 @@
-from fabric.api import local
+from invoke import  task, run
 from jinja2 import Environment, FileSystemLoader
 
 
 template_env = Environment(loader=FileSystemLoader('.'))
 
-
+@task
 def build():
     template = template_env.get_template('source/index.html')
     rendered_template = template.render()
-    with open('presentation/index.html', 'wb') as fh:
+    with open('presentation/index.html', 'w') as fh:
         fh.write(rendered_template)
 
+@task(build)
 def publish():
-    build()
-    local('ghp-import -p presentation')
+    run('ghp-import -p presentation')
