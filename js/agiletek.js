623a863a1ef3e54f7fb832208ce217c99c6c23db
srossiter <steve@agiletek.co.uk>
2015-06-04 13:38:11 +0100

    added navbar and other goodies

--- js/agiletek.js
+++ js/agiletek.js
@@ -1,3 +1,14 @@
 /**
  * Created by stevenrossiter on 03/06/2015.
  */
+
+$(function(){
+
+    $("#overview").click(function(){
+        Reveal.toggleOverview();
+    });
+    Reveal.addEventListener( 'slidechanged', function( event ) {
+        $('.nav-link').css("background-color", "");
+    $("#h-"+event.indexh).css("background-color", "rgba(255,255,255,0.3)");
+} );
+});
